21cab37bb1aee82c5ccc0e4b263ee4902445ece3
Zeyad Hyman <147080913+ZeyadHyman@users.noreply.github.com>
2025-02-13 20:14:29 +0200

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Home() {
 
   return (
     <div
-      className={`bottom-0 fixed w-full md:hidden bg-[#c3aa6a]/90 backdrop-blur-xl shadow-md flex justify-between items-center py-4 px-5 transition-all duration-500 ${
+      className={`bottom-0 fixed w-full md:hidden bg-[#c3aa6a]/90 backdrop-blur-xl shadow-md flex justify-between items-center px-6 py-4 transition-all duration-500 ${
         isLoaded ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"
       }`}
     >
@@ -29,15 +29,15 @@ function Home() {
               index !== arr.length - 1 ? "border-r border-white/40 pr-3" : ""
             }`}
           >
-            <span className="text-base font-bold">{value}</span>
-            <span className="text-xs uppercase tracking-widest text-gray-100">
+            <span className="text-sm font-bold">{value}</span>
+            <span className="text-[10px] uppercase tracking-widest text-gray-100">
               {key}
             </span>
           </div>
         ))}
       </div>
 
-      <button className="bg-[#2E2E2E] text-white px-4 py-3 rounded-xl text-sm font-semibold shadow-md transition-all duration-300 active:bg-[#444] active:scale-95">
+      <button className="bg-[#2E2E2E] text-white px-3 py-3 rounded-xl text-xs font-semibold shadow-md transition-all duration-300 active:bg-[#444] active:scale-95">
         Get a Ticket
       </button>
     </div>
